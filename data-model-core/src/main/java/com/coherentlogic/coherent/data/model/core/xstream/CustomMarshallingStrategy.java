ce1333512a44f72062ebc5047fe9829ee682b8cb
thospfuller <thomas.fuller@coherentlogic.com>
2016-03-17 12:24:33 -0400

    Minor refactoring and formatting, also changed a try/catch block in the assignPropertyChangeSupport method.

--- data-model-core/src/main/java/com/coherentlogic/coherent/data/model/core/xstream/CustomMarshallingStrategy.java
+++ data-model-core/src/main/java/com/coherentlogic/coherent/data/model/core/xstream/CustomMarshallingStrategy.java
@@ -19,43 +19,44 @@ import com.thoughtworks.xstream.io.HierarchicalStreamReader;
 import com.thoughtworks.xstream.mapper.Mapper;
 
 /**
- * A marshalling strategy that assigns an instance of the {@link
- * #propertyChangeSupportType} to any object which extends from {@link
- * com.coherentlogic.coherent.data.model.core.domain.SerializableBean}.
+ * A marshalling strategy that assigns an instance of the {@link #propertyChangeSupportType} to any object which extends
+ * from {@link com.coherentlogic.coherent.data.model.core.domain.SerializableBean}.
  *
- * The CustomMarshallingStrategy is set on the XStreamMarshaller as follows:
+ * The CustomMarshallingStrategy is set as a property on the XStreamMarshaller as follows:
  * <pre>
  * XStreamMarshaller result = new XStreamMarshaller ();
  *
  * result.setMarshallingStrategy(new CustomMarshallingStrategy ());
  * </pre>
  *
+ * or in XML:
+ * <pre>
+ * <bean id="xstreamMarshaller" class="org.springframework.oxm.xstream.XStreamMarshaller">
+ *     <property name="marshallingStrategy">
+ *         <bean class="com.coherentlogic.coherent.data.model.core.xstream.CustomMarshallingStrategy"/>
+ *     </property>
+ *     ...
+ * </bean>
+ * </pre>
  * @author <a href="mailto:support@coherentlogic.com">Support</a>
  */
-public class CustomMarshallingStrategy extends
-    ReferenceByXPathMarshallingStrategy {
+public class CustomMarshallingStrategy extends ReferenceByXPathMarshallingStrategy {
 
-    private static final Logger log = LoggerFactory
-        .getLogger(CustomMarshallingStrategy.class);
+    private static final Logger log = LoggerFactory.getLogger(CustomMarshallingStrategy.class);
 
-    private final Class<? extends PropertyChangeSupport>
-        propertyChangeSupportType;
+    private final Class<? extends PropertyChangeSupport> propertyChangeSupportType;
 
     /**
-     * A default constructor that sets the mode to {@link
-     * ReferenceByXPathMarshallingStrategy#RELATIVE} and the
-     * {@link #propertyChangeSupportType} to {@link
-     * java.beans.PropertyChangeSupport}.
+     * A default constructor that sets the mode to {@link ReferenceByXPathMarshallingStrategy#RELATIVE} and the
+     * {@link #propertyChangeSupportType} to {@link java.beans.PropertyChangeSupport}.
      */
     public CustomMarshallingStrategy() {
         this (RELATIVE, PropertyChangeSupport.class);
     }
 
     /**
-     * A constructor that sets the mode to {@link
-     * ReferenceByXPathMarshallingStrategy#RELATIVE} and the
-     * {@link #propertyChangeSupportType} to the propertyChangeSupportType
-     * parameter value.
+     * A constructor that sets the mode to {@link ReferenceByXPathMarshallingStrategy#RELATIVE} and the
+     * {@link #propertyChangeSupportType} to the propertyChangeSupportType parameter value.
      */
     public CustomMarshallingStrategy(
         Class<? extends PropertyChangeSupport> propertyChangeSupportType
@@ -64,22 +65,18 @@ public class CustomMarshallingStrategy extends
     }
 
     /**
-     * A constructor that sets the mode to the value parameter value and the
-     * {@link #propertyChangeSupportType} to the propertyChangeSupportType
-     * parameter value.
+     * A constructor that sets the mode to the value parameter value and the {@link #propertyChangeSupportType} to the
+     * propertyChangeSupportType parameter value.
      */
-    public CustomMarshallingStrategy(
-        int mode,
-        Class<? extends PropertyChangeSupport> propertyChangeSupportType
-    ) {
+    public CustomMarshallingStrategy(int mode, Class<? extends PropertyChangeSupport> propertyChangeSupportType) {
         super(mode);
         this.propertyChangeSupportType = propertyChangeSupportType;
     }
 
     /**
-     * Method assigns an instance of {@link java.beans.PropertyChangeSupport} to
-     * all objects of type {@link
+     * Method assigns an instance of {@link java.beans.PropertyChangeSupport} to all objects of type {@link
      * com.coherentlogic.coherent.data.model.core.domain.SerializableBean}.
+     *
      * @throws NoSuchFieldException 
      *
      * @deprecated Recent changes to the {@link SerializableBean} such that the PropertyChangeSupport reference should
@@ -94,10 +91,8 @@ public class CustomMarshallingStrategy extends
     }
 
     /**
-     * Method assigns an instance of {@link java.beans.PropertyChangeSupport} to
-     * the {@link defaultObject
-     * com.coherentlogic.coherent.data.model.core.domain.SerializableBean}
-     * parameter.
+     * Method assigns an instance of {@link java.beans.PropertyChangeSupport} to the {@link defaultObject
+     * com.coherentlogic.coherent.data.model.core.domain.SerializableBean} parameter.
      *
      * @throws NoSuchFieldException 
      */
@@ -128,8 +123,6 @@ public class CustomMarshallingStrategy extends
             PropertyChangeSupport propertyChangeSupport = (PropertyChangeSupport) constructor.newInstance(
                 serializableBean);
 
-            try {
-
                 Field propertyChangeSupportField = SerializableBean.class.getField("propertyChangeSupport");
 
                 propertyChangeSupportField.setAccessible(true);
@@ -138,11 +131,9 @@ public class CustomMarshallingStrategy extends
 
                 propertyChangeSupportField.setAccessible(false);
 
-            } catch (NoSuchFieldException noSuchFieldException) {
-                throw new GenericReflectionException("The propertyChangeSupport field set operation failed so this "
-                    + "property may have been renamed or removed altogether.", noSuchFieldException);
-            }
-
+        } catch (NoSuchFieldException noSuchFieldException) {
+            throw new GenericReflectionException("The propertyChangeSupport field set operation failed so this "
+                + "property may have been renamed or removed altogether.", noSuchFieldException);
         } catch (
             NoSuchMethodException |
             SecurityException |
