18cf9ef66b6f472bd0416c5d29d6231a7d1a1715
thospfuller <thomas.fuller@coherentlogic.com>
2016-03-28 17:14:15 -0400

    Added documentation regarding applying the JAMON performance interceptor.

--- data-model-core/src/main/java/com/coherentlogic/coherent/data/model/core/builders/CacheableQueryBuilder.java
+++ data-model-core/src/main/java/com/coherentlogic/coherent/data/model/core/builders/CacheableQueryBuilder.java
@@ -3,7 +3,27 @@ package com.coherentlogic.coherent.data.model.core.builders;
 import com.coherentlogic.coherent.data.model.core.cache.CacheServiceProviderSpecification;
 
 /**
- * 
+ * Apply the JAMON Performance Interceptor as follows:
+ *
+ * <pre>
+ * <bean id="springMonitoringAspectInterceptor"
+ *  class="org.springframework.aop.interceptor.JamonPerformanceMonitorInterceptor">
+ *     <property name="trackAllInvocations" value="true"/>
+ *     <property name="useDynamicLogger" value="true"/>
+ * </bean>
+ *
+ * <aop:config proxy-target-class="true">
+ *     <aop:advisor advice-ref="springMonitoringAspectInterceptor"
+ *      pointcut=
+ *      "execution(public * com.coherentlogic.coherent.data.model.core.builders.HttpMethodsSpecification.doGet(..)))"/>
+ * </aop:config>
+ * </pre>
+ *
+ * The important item to note here is that the pointcut is applied to the </b>interface<b> and not the implementation --
+ * if you target the implementation nothing will appear in the log.
+ *
+ * @todo Auto-apply the performance interceptor.
+ *
  * @author <a href="mailto:support@coherentlogic.com">Support</a>
  */
 public abstract class CacheableQueryBuilder<K, V> {
