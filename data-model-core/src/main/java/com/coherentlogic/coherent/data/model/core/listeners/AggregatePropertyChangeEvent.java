9a84a7162950e4174d3b8d1d55e1216d0a916a53
thospfuller <thomas.fuller@coherentlogic.com>
2016-08-12 14:40:07 -0400

    Added a type to the class, added a source param to the constructor, and added a source getter method. The hashCode, equals, and toString methods have been regenerated.

--- data-model-core/src/main/java/com/coherentlogic/coherent/data/model/core/listeners/AggregatePropertyChangeEvent.java
+++ data-model-core/src/main/java/com/coherentlogic/coherent/data/model/core/listeners/AggregatePropertyChangeEvent.java
@@ -7,17 +7,32 @@ import java.util.Map;
 /**
  * Event which is fired that contains all events generated from one discrete update.
  *
+ * @param <S> The source type.
+ *
  * @author <a href="https://www.linkedin.com/in/thomasfuller">Thomas P. Fuller</a>
  * @author <a href="mailto:support@coherentlogic.com">Support</a>
  */
-public class AggregatePropertyChangeEvent implements Serializable {
+public class AggregatePropertyChangeEvent<S> implements Serializable {
 
-    private static final long serialVersionUID = -7524083743818359273L;
+    private static final long serialVersionUID = 7169677559654674929L;
+
+    private final S source;
 
     private final Map<String, PropertyChangeEvent> propertyChangeEventMap;
 
-    public AggregatePropertyChangeEvent(Map<String, PropertyChangeEvent> propertyChangeEventMap) {
+    public AggregatePropertyChangeEvent(S source, Map<String, PropertyChangeEvent> propertyChangeEventMap) {
+        this.source = source;
         this.propertyChangeEventMap = propertyChangeEventMap;
+    }
+
+    /**
+     * Getter method for the source, which caused this event to be fired.
+     */
+    public S getSource() {
+        /* Note that we could take the source from any of the PropertyChangeEvent instances and it will suffice but this
+         * isn't really clean (IMO) hence we've added this method here..
+         */
+        return source;
     }
 
     public Map<String, PropertyChangeEvent> getPropertyChangeEventMap() {
@@ -29,6 +44,7 @@ public class AggregatePropertyChangeEvent implements Serializable {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((propertyChangeEventMap == null) ? 0 : propertyChangeEventMap.hashCode());
+        result = prime * result + ((source == null) ? 0 : source.hashCode());
         return result;
     }
 
@@ -46,11 +62,17 @@ public class AggregatePropertyChangeEvent implements Serializable {
                 return false;
         } else if (!propertyChangeEventMap.equals(other.propertyChangeEventMap))
             return false;
+        if (source == null) {
+            if (other.source != null)
+                return false;
+        } else if (!source.equals(other.source))
+            return false;
         return true;
     }
 
     @Override
     public String toString() {
-        return "AggregatePropertyChangeEvent [propertyChangeEventMap=" + propertyChangeEventMap + "]";
+        return "AggregatePropertyChangeEvent [source=" + source + ", propertyChangeEventMap=" + propertyChangeEventMap
+            + "]";
     }
 }
--- data-model-core/src/main/java/com/coherentlogic/coherent/data/model/core/listeners/AggregatePropertyChangeListener.java
+++ data-model-core/src/main/java/com/coherentlogic/coherent/data/model/core/listeners/AggregatePropertyChangeListener.java
@@ -5,11 +5,13 @@ import java.util.EventListener;
 /**
  * Specification that is implemented when listening for aggregate property change event notifications.
  *
+ * @param <S> The aggregatePropertyChangeEvent source type.
+ *
  * @author <a href="https://www.linkedin.com/in/thomasfuller">Thomas P. Fuller</a>
  * @author <a href="mailto:support@coherentlogic.com">Support</a>
  */
 @FunctionalInterface
-public interface AggregatePropertyChangeListener extends EventListener {
+public interface AggregatePropertyChangeListener<S> extends EventListener {
 
-    void onAggregatePropertyChangeEvent (AggregatePropertyChangeEvent aggregatePropertyChangeEvent);
+    void onAggregatePropertyChangeEvent (AggregatePropertyChangeEvent<S> aggregatePropertyChangeEvent);
 }
