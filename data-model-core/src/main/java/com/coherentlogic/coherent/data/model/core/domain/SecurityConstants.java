c4c5547d2af051c6a819895ccda9ebf9bb91e6b9
thospfuller <thomas.fuller@coherentlogic.com>
2016-11-18 12:51:30 -0500

    New interface added.

---
+++ data-model-core/src/main/java/com/coherentlogic/coherent/data/model/core/domain/SecurityConstants.java
@@ -0,0 +1,27 @@
+package com.coherentlogic.coherent.data.model.core.domain;
+
+public interface SecurityConstants {
+
+    public static final String
+        PRIVILEGE = "privilege",
+        PRIVILEGES = "privileges",
+        NAME = "name",
+        ROLE = "role",
+        ROLES = "roles",
+        ROLE_PRIVILEGES = "role_privileges",
+        ROLE_ID = "role_id",
+        PRIVILEGE_KEY = "privilege_key",
+        USERS = "users",
+        USER = "user",
+        FIRST_NAME = "firstName",
+        LAST_NAME = "lastName",
+        EMAIL = "email",
+        PASSWORD = "password",
+        ENABLED = "enabled",
+        TOKEN_EXPIRED = "tokenExpired",
+        USER_ROLES = "user_roles",
+        USER_KEY = "user_key",
+        ROLE_KEY = "role_key",
+        PRIMARY_KEY = "primaryKey",
+        USERS_ROLLS = "users_rolls";
+}
